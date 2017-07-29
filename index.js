750577746f87b9aff39c50551b7c43bfe5f9fe43
morishitter <morishitter@gmail.com>
2017-07-29 14:10:51 +0900

    :golf:

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 var h = require("picodom").h
 
-var uid = 0
+var x = 0
 var insert = n => n
 var cache = {}
 
 if (typeof window !== "undefined") {
   var sheet = document.head.appendChild(document.createElement("style")).sheet
-  insert = rule => sheet.insertRule(rule, sheet.cssRules.length)
+  insert = r => sheet.insertRule(r, sheet.cssRules.length)
 }
 
 var setpx = v => typeof v === "number" ? `${v}px` : v
 var propertize = s => s.replace(/[A-Z]|^ms/g, "-$&").toLowerCase()
 
-var createRule = (cn, p, v, m) => {
-  var rule = `.${cn}{ ${propertize(p)}: ${setpx(v)} }`
-  return m ? `${m}{${rule}}` : rule
+var rulize = (cn, p, v, m) => {
+  var r = `.${cn}{ ${propertize(p)}: ${setpx(v)} }`
+  return m ? `${m}{${r}}` : r
 }
 
-function parse (obj, child = "", m) {
-  return Object.keys(obj).map(p => {
-    var v = obj[p]
+function scan (o, c = "", m) {
+  return Object.keys(o).map(p => {
+    var v = o[p]
     if (v === null) return ""
 
     if (typeof v === "object") {
-      var nestedMedia = /^@/.test(p) ? p : null
-      var nestedClass = nestedMedia ? child : child + p
-      return parse(v, nestedClass, nestedMedia)
+      var nm = /^@/.test(p) ? p : null
+      var nc = nm ? c : c + p
+      return scan(v, nc, nm)
     }
 
     var key = p + v + m
     if (cache[key]) return cache[key]
 
-    var cn = "p" + (uid++).toString(36)
-    insert(createRule(cn + child, p, v, m))
+    var cn = "p" + (x++).toString(36)
+    insert(rulize(cn + c, p, v, m))
     cache[key] = cn
 
     return cn
   }).join(" ")
 }
 
-module.exports = tag => (...args) => (_, children) => h(tag, { class: args.map(arg => parse(arg)).join(" ") }, children)
+module.exports = tag => (...args) => (_, cs) => h(tag, { class: args.map(arg => scan(arg)).join(" ") }, cs)
