f28f0bbc55638d22de01b234ebc5105337047e3a
dominuskernel <promodominus@gmail.com>
2018-03-10 11:55:09 +0100

    add types for picostyle

--- picostyle.d.ts
+++ picostyle.d.ts
@@ -1,26 +1,25 @@
 export type Children = VNode | string | number | null;
 
 interface VNode<Attributes = {}> {
-    nodeName: string
-    attributes?: Attributes
-    children: Array<VNode | string>
-    key: string
+    nodeName: string;
+    attributes?: Attributes;
+    children: Array<VNode | string>;
+    key: string;
 }
 
-interface Component<Attributes = {}> {
-    (attributes: Attributes, children: Array<VNode | string>): VNode<Attributes>
-}
+type Component<Attributes = {}> = (attributes: Attributes, children: Array<VNode | string>) => VNode<Attributes>;
 
 type Keys = string;
 
-type createNode = <Attributes>(nodeName: Component<Attributes> | string, attributes?: Attributes | null,
-    ...children: Array<Children | Children[]>) => VNode<Attributes>
+type createNode = <Attributes>(nodeName: Component<Attributes> | string,
+                               attributes?: Attributes | null,
+                               ...children: Array<Children | Children[]>) => VNode<Attributes>;
 
-type StyleProps = (props: any) => IStyles;
+type StyleProps = (props: any) => Styles;
 
-interface IStyles {
+interface Styles {
     [key: string]: any;
 }
 
-export function picostyle(vdom: createNode): (element: string | Component<Object>) =>
-    (styles: IStyles | StyleProps) => (...children: any[]) => VNode | string | number | null
+export function picostyle(vdom: createNode): (element: string | Component<object>) =>
+    (styles: Styles | StyleProps) => (...children: any[]) => VNode | string | number | null;
