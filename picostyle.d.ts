611b43647966b210098beef43f71f52b450aa028
Marais Rossouw <me@maraisr.com>
2020-05-07 10:02:18 +1000

    chore: Support nested styles also

--- picostyle.d.ts
+++ picostyle.d.ts
@@ -9,15 +9,20 @@ interface VNode<Attributes = {}> {
     key: string;
 }
 
+interface NestedStyles {
+  [key: string]: Styles;
+}
+
 type Component<Attributes = {}> = (attributes: Attributes, children: Array<VNode | string>) => VNode<Attributes>;
 
 type createNode = <Attributes>(nodeName: Component<Attributes> | string,
                                attributes?: Attributes | null,
                                ...children: Array<Children | Children[]>) => VNode<Attributes>;
 
-type StyleProps = (props: any) => Styles;
+type StyleProps = (props: any) => Styles | NestedStyles;
+type StyleObject = Styles | StyleProps | NestedStyles;
 
 export const keyframes: (obj: StyleProps) => string;
 
 export default function picostyle(vdom: createNode): (element: string | Component<object>) =>
-    (styles: Styles | StyleProps) => (...children: any[]) => VNode | string | number | null;
+    (styles: StyleObject) => (...children: any[]) => VNode | string | number | null;
